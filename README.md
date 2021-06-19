f1c83250be11de65179d3109e7d0603e13d8d879
galaxy-s10 <2274751790@qq.com>
2021-06-19 21:29:51 +0800

    0619

--- README.md
+++ README.md
@@ -1,22 +1,20 @@
-# client
-
-> My amazing Nuxt.js project
+# nuxtblog
 
 ## Build Setup
 
-``` bash
+```bash
 # install dependencies
-$ npm install
+$ yarn install
 
 # serve with hot reload at localhost:3000
-$ npm run dev
+$ yarn dev
 
 # build for production and launch server
-$ npm run build
-$ npm run start
+$ yarn build
+$ yarn start
 
 # generate static project
-$ npm run generate
+$ yarn generate
 ```
 
 For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,82 +1,69 @@
-
 export default {
-  mode: 'universal',
-  /*
-  ** Headers of the page
-  */
+  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || "",
+    // htmlAttrs: {
+    //   lang: "en"
+    // },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
-      { name: 'description', content: "前端 | 大前端 | 开源 | 自然博客" },
-      { name: 'keywords', content: "自然博客" },
-      { property: 'og:type', content: "website" },
-      { property: 'og:title', content: "前端 | 大前端 | 开源 | 自然博客" },
-      { property: 'og:url', content: "https://www.zhengbeining.com" },
-      { property: 'og:site_name', content: "前端 | 大前端 | 开源 | 自然博客" },
-      { property: 'og:description', content: "前端 | 大前端 | 开源 | 自然博客" }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
+      },
+      { name: "description", content: "前端 | 大前端 | 开源 | 自然博客" },
+      { name: "keywords", content: "自然博客" },
+      { property: "og:type", content: "website" },
+      { property: "og:title", content: "前端 | 大前端 | 开源 | 自然博客" },
+      { property: "og:url", content: "https://www.zhengbeining.com" },
+      { property: "og:site_name", content: "前端 | 大前端 | 开源 | 自然博客" },
+      { property: "og:description", content: "前端 | 大前端 | 开源 | 自然博客" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
+
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#53a8ff' },
-  /*
-  ** Global CSS
-  */
-  css: [
-    'element-ui/lib/theme-chalk/index.css',
-    '~/assets/css/main.css'
-  ],
-  /*
-  ** Plugins to load before mounting the App
-  */
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#53a8ff" },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: ["element-ui/lib/theme-chalk/index.css", "~/assets/css/main.css"],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '@/plugins/axios',
-    '@/plugins/element-ui',
-    '@/plugins/vue-lazyload',
-    { src: '~/plugins/tip' },
+    "@/plugins/axios",
+    "@/plugins/element-ui",
+    "@/plugins/vue-lazyload",
+    { src: "~/plugins/tip" }
   ],
-  /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-  ],
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    '@nuxtjs/axios',
-    '@nuxtjs/proxy'
-  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: ["@nuxtjs/axios", "@nuxtjs/proxy"],
   server: {
-    port: 3000, // default: 3000
+    port: 3000 // default: 3000
   },
   axios: {
-    proxy: true,
+    proxy: true
   },
   proxy: {
-    '/api': {
-      target: 'http://www.zhengbeining.com:3003',
+    "/api": {
+      target: "http://42.193.157.44/api",
       // target: 'http://localhost:3003',
       pathRewrite: {
-        '^/api': ''
+        "^/api": ""
       }
     }
   },
-  /*
-  ** Build configuration
-  */
-  build: {
-    /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-    }
-  }
-}
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {}
+};

bddbe704a4f00014af6433c5b384d05ba0bc1a71
shuisheng <2274751790@qq.com>
2024-01-29 12:51:15 +0800

    fix: safari时间戳

--- src/utils/format.js
+++ src/utils/format.js
@@ -2,9 +2,10 @@
  * 获取日期当天的开始时间到结束时间
  */
 export function dateStartAndEnd(date) {
+  const addZero = (v) => (v < 10 ? `0${v}` : v);
   const y = date.getFullYear();
-  const m = date.getMonth() + 1;
-  const d = date.getDate();
+  const m = addZero(date.getMonth() + 1);
+  const d = addZero(date.getDate());
   const startTime = `${y}-${m}-${d} 00:00:00`;
   const endTime = `${y}-${m}-${d} 23:59:59`;
   return {
